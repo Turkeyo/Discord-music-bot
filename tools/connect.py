b0bf1df782a23094f6fcce8c3dbf938abca94e1c
Turkeyo <89434210+Turkeyo@users.noreply.github.com>
2024-04-28 12:37:58 +0800

    Update connect.py

--- tools/connect.py
+++ tools/connect.py
@@ -1,6 +1,15 @@
-import asyncio
-async def conn(ctx,self):
-    print('First to connect the channel')
-    #connect the channel (We will see the bot in the voice channel)
-    self.voice_channel = await ctx.author.voice.channel.connect()
-    self.voice_clients[self.voice_channel.guild.id] = self.voice_channel
+from tools.card import inf_play
+class connect():
+    async def conn(ctx,self):
+        print('First to connect the channel')
+        #connect the channel (We will see the bot in the voice channel)
+        self.voice_channel = await ctx.author.voice.channel.connect()
+        self.voice_clients[self.voice_channel.guild.id] = self.voice_channel
+
+    async def dis_conn(ctx,self):
+        print('Disconnect the channel')
+        #connect the channel (We will see the bot in the voice channel)
+        await self.voice_clients[ctx.guild.id].disconnect()
+        title_msg = "Leave voice channel..."
+        await ctx.send(embed=inf_play(ctx,title_msg=title_msg))
+        
