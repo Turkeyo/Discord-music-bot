f841736010ecaa621e491b2a0df613379154768f
Turkeyo <89434210+Turkeyo@users.noreply.github.com>
2024-04-28 12:38:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,16 @@ from discord.flags import Intents
 
 Intents = discord.Intents.all()
 Intents.message_content = True
+
 # Set prefix
 bot = commands.Bot(command_prefix="!", intents=Intents)
 # Remove initialization command 'help'
 bot.remove_command("help")
 
 # Online bot
-
-
 @bot.event
 async def on_ready():
-    print(f"{bot.user} is ready")
+        print(f"{bot.user} is ready")
 
 # Load commands
 async def main():
@@ -25,10 +24,10 @@ async def main():
         for Filename in os.listdir("./cmds"):
             if Filename.endswith(".py"):
                 await bot.load_extension(f"cmds.{Filename[:-3]}")
+                print(f"Load {Filename[:-3]} success...")
         load_dotenv()
         TOKEN = os.getenv("discord_token")
         await bot.start(TOKEN)
-
 
 if __name__ == "__main__":
     asyncio.run(main())
