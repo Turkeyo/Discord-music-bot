72b11309d93d6e0c56c28067ffa9986ce7a1905b
Turkeyo <89434210+Turkeyo@users.noreply.github.com>
2024-04-28 12:38:03 +0800

    Update maincog.py

--- core/maincog.py
+++ core/maincog.py
@@ -1,6 +1,4 @@
-import discord
 from discord.ext import commands
-
 
 class MainCog(commands.Cog):
 
@@ -8,15 +6,13 @@ class MainCog(commands.Cog):
         self.bot = bot
 
         self.play_count = 0
-
         self.isplaying = False
         self.ispaused = False
 
         self.voice_channel = None
         self.voice_clients = {}
         self.music_queue = []     
+
+        #Music settings
         self. yt_dl_options = {'format': 'bestaudio/best','before_options': '-reconnect 1 -reconnected_streamed 1  -reconnect_delay_max 5','options': '-vn'}
         self.ffmpeg_options = {'options': '-vn',"before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"}
-        #self. yt_dl_options = {"format": "bestaudio/best"}
-        #self.ffmpeg_options = {'format':'bestaudio','noplaylist':'True'}
-        self.vc = False
